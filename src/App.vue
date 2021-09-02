921a17a3fc64c2e2ad60f29875c781c569c6b783
wuhaojian <wuhaojian@bytedance.com>
2021-09-02 15:25:00 +0800

    feat: 不用路由

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <router-view />
+  <div class="hello">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <h1>是不是闰年呢？</h1>
+    <hr />
+    <van-field v-model="year" label="年 份" placeholder="请输入年份" label-width="50px" />
+    <h2>{{ res }}</h2>
+  </div>
 </template>
-<script lang="ts"></script>
 
-<style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      year: '2000',
+    };
+  },
+  computed: {
+    res() {
+      const y = Number(this.year);
+      console.log(y);
+
+      if (Number.isNaN(y) || y < 0) {
+        return '你输入的是啥？？？年份吗？';
+      } else {
+        if ((y % 4 === 0 && y % 100 !== 0) || (y % 100 === 0 && y % 400 === 0)) {
+          return '是闰年';
+        } else {
+          return '不是闰年';
+        }
+      }
+    },
+  },
+});
+</script>
+
+<style scoped>
 #app {
-  background-image: url('https://up.enterdesk.com/edpic_source/8e/d7/f3/8ed7f3c4a2910809b49ec71c7d7ca1f0.jpg');
-  background-size: 100% 100%;
-
-  /* height: 667px;
-  width: 375px; */
-  height: fit-content;
-  width: 100%;
-  text-align: center;
-  color: #fdd806;
 }
 </style>
